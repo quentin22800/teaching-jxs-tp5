3118be8f84999e6b977d7b66e481af1f5c4d3490
quentin eono <17010063@e105m04.istic.univ-rennes1.fr>
2018-01-25 11:46:21 +0100

    q12

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,3 +6,26 @@ pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+pokeApp.factory('ServicePokeOne', ['$resource',function($resource) {
+  return $resource("https://pokeapi.co/api/v2/pokemon/:id/");
+}]);
+
+pokeApp.controller('controllerpokedex', ['$scope','$log', 'ServicePokeOne', function($scope, $log, ServicePokeOne) {
+  
+  $scope.data = {
+  	listepoke:[
+  		{id: '1', name : 'bulbisar'},
+  		{id: '2', name: 'salamèche'},
+  		{id: '3', name: 'pikachu'},
+  		{id: '4', name: 'dracaufeu'}
+  ]};
+
+  $scope.validerchoix = function(){
+  	$log.log(ServicePokeOne.get({id:$scope.choixpoke}));
+
+  };
+
+
+}]);
+
