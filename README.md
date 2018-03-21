1b0a1b0e65890b6fdca682a02a03e3e33fd97a2b
goaillo <gaelrebours@gmail.com>
2018-03-21 09:48:14 +0100

    Refacto pokeApi + added moves

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# teaching-jxs-tp5
-TP5 : AngularJS
+# TP 6 SIR 2018 AngularJS PokeAPI
+## How to install 
+    Launch the app via : ```index.html ``` (sometimes don't work on chrome)
+## How to use
+    Enter the name of the pokemon you want and then search his name in the spinner. 
+    It will show you his front sprite and his different moves
--- js/pokedex.js
+++ js/pokedex.js
@@ -22,21 +22,30 @@ pokeApp.directive('pokedex', function(){
 });
 
 
-pokeApp.controller('controllerpokedex', ['$scope','$log', 'ServicePokeOne','ServiceInfoPoke' , function($scope, $log, ServicePokeOne, ServiceInfoPoke) {
+pokeApp.controller('controllerpokedex', ['$scope','$log', 'ServicePokeOne','ServiceInfoPoke', '$http' , function($scope, $log, ServicePokeOne, ServiceInfoPoke, $http) {
   
-  $scope.data = {
+  /*$scope.data = {
   	listepoke:[
   		{id: '1', name: 'bulbisar'},
   		{id: '2', name: 'salamèche'},
   		{id: '3', name: 'pikachu'},
   		{id: '4', name: 'dracaufeu'}
-  ]};
+  ]};*/
+  $http.get('https://pokeapi.co/api/v2/pokemon/?limit=802&offset=0').then(function(response)
+  {
+     $scope.listepoke = response.data.results;
+     $scope.listepoke.forEach(function(e){ e.id = e.url.replace('https://pokeapi.co/api/v2/pokemon/','').replace('/','')});
+     console.log($scope.listepoke);
+  }, function(response){
+      console.log(response);
+  });
 
   $scope.validerchoix = function(){
       $scope.pokemon = ServicePokeOne.get({id:$scope.choixpoke}, function(){
           ServiceInfoPoke.value = $scope.pokemon;
         }
       );
+      $log.log($scope.pokemon);
   };
 
 }]);
